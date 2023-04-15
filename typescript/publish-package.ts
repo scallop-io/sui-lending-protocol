f1384465799cd312a7f49d8d598eccb65fc9e65e
icefox.eth <zengxinhai001@163.com>
2023-04-16 01:47:33 +0800

    update: new package publisher

--- typescript/publish-package.ts
+++ typescript/publish-package.ts
@@ -1,20 +1,17 @@
 import path from "path";
 import dotenv from "dotenv";
 import { SuiKit } from "@scallop-dao/sui-kit";
+import { SuiPackagePublisher } from "@scallop-dao/sui-package-kit";
 dotenv.config();
 
 (async() => {
   const mnemonics = process.env.MNEMONICS;
   const networkType = (process.env.NETWORK_TYPE || 'devnet') as 'devnet' | 'testnet' | 'mainnet';
   const suiKit = new SuiKit({ mnemonics, networkType });
-  const balance = await suiKit.getBalance();
-  if (balance.totalBalance <= 3000) {
-    await suiKit.requestFaucet();
-  }
-  // Wait for 3 seconds before publish package
-  await new Promise(resolve => setTimeout(() => resolve(true), 3000));
 
   const packagePath = path.join(__dirname, '../query');
-  const result = await suiKit.publishPackage(packagePath);
+  const publisher = new SuiPackagePublisher();
+  const signer = suiKit.getSigner();
+  const result = await publisher.publishPackage(packagePath, signer);
   console.log('packageId: ' + result.packageId);
 })();

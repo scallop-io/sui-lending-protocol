71f60aee721df96a09f5ce9a75b03a890722feaf
icefox <zengxinhai001@163.com>
2023-05-07 20:17:21 +0800

    Feat: integrate x oracle

--- typescript/package-publish/publish-protocol.ts
+++ typescript/package-publish/publish-protocol.ts
@@ -1,17 +1,13 @@
 import * as path from "path";
 import { RawSigner } from "@mysten/sui.js";
 import { PackagePublishResult } from "@scallop-dao/sui-package-kit";
-import { publishPackageWithCache, publishPackageEnforce, cleanAfterPublish } from "./publish-packages";
-import { extractObjects } from "./extract-objects-from-publish-results";
+import { publishPackageWithCache } from "./publish-packages";
 import { suiKit, networkType } from "../sui-kit-instance";
 
 const mathPkgPath = path.join(__dirname, "../../math");
 const xPkgPath = path.join(__dirname, "../../x");
 const whitelistPkgPath = path.join(__dirname, "../../whitelist");
-const switchboardPkgPath = path.join(__dirname, "../../switchboard");
 const testCoinPkgPath = path.join(__dirname, "../../test_coin");
-const testSwitchboardAggregatorPkgPath = path.join(__dirname, "../../test_switchboard_aggregator");
-const oraclePkgPath = path.join(__dirname, "../../oracle");
 const protocolPkgPath = path.join(__dirname, "../../protocol");
 const protocolQueryPkgPath = path.join(__dirname, "../../query");
 
@@ -19,12 +15,9 @@ const protocolPackageList = [
   { pkgPath: mathPkgPath },
   { pkgPath: xPkgPath },
   { pkgPath: whitelistPkgPath },
-  { pkgPath: switchboardPkgPath, placeholderNames: ['switchboard'], enableCache: true },
-  { pkgPath: testCoinPkgPath, placeholderNames: ['test_coin'] },
-  { pkgPath: testSwitchboardAggregatorPkgPath, placeholderNames: ['test_switchboard_aggregator'] },
-  { pkgPath: oraclePkgPath },
-  { pkgPath: protocolPkgPath },
-  { pkgPath: protocolQueryPkgPath, placeholderNames: ['protocol_query'] }
+  { pkgPath: testCoinPkgPath },
+  // { pkgPath: protocolPkgPath },
+  // { pkgPath: protocolQueryPkgPath }
 ];
 export const publishProtocol = async (
   signer: RawSigner,
@@ -41,24 +34,9 @@ export const _publishProtocol = async (
   const publishResults: { publishResult: PackagePublishResult, packageName: string }[] = [];
   for (const pkg of packagePathList) {
     const pkgPath = pkg.pkgPath;
-    const placeholderNames = pkg.placeholderNames || [];
-    const enableCache = pkg.enableCache ? pkg.enableCache : false;
-    const res = enableCache
-      ? await publishPackageWithCache(pkgPath, placeholderNames, signer, networkType)
-      : await publishPackageEnforce(pkgPath, placeholderNames, signer, networkType)
+    const res = await publishPackageWithCache(pkgPath, signer, networkType)
     res && publishResults.push(res);
   }
-  const ids = extractObjects(publishResults);
-  cleanTomlsAfterPublish(packagePathList);
-  return ids;
 }
 
-const cleanTomlsAfterPublish = (
-  packagePathList: { pkgPath: string, placeholderNames?: string[] }[]
-) => {
-  for (const pkg of packagePathList) {
-    const pkgPath = pkg.pkgPath;
-    const placeholderNames = pkg.placeholderNames || [];
-    cleanAfterPublish(pkgPath, placeholderNames);
-  }
-}
+publishProtocol(suiKit.getSigner()).then(console.log).catch(console.error);

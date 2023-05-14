ff3f2dae7672c5c7d3aa0cd33e568f4b8cccb2ca
icefox <zengxinhai001@163.com>
2023-05-14 22:12:19 +0800

    Update: add support for supra

--- contracts/sui_x_oracle/oracle-price.ts
+++ contracts/sui_x_oracle/oracle-price.ts
@@ -1,25 +1,24 @@
+import * as path from "path";
 import { SuiTxBlock, SuiTxArg } from "@scallop-io/sui-kit"
 import { testCoinTypes } from "contracts/test_coin"
 import {
   xOracleTxBuilder,
-  pythRuleTxBuilder,
-  pythTestnetIds,
   switchboardRuleTxBuilder,
-  switchboardTestnetIds,
 } from "./index"
+
 import { getVaas } from "./get-vaas"
 
-export const updatePythPriceForRequest = async (tx: SuiTxBlock, updateRequest: SuiTxArg, coinType: string) => {
-  const priceFeed = getPythPriceFeed(coinType);
-  const [vaa] = await getVaas([priceFeed.priceFeedId]);
-  pythRuleTxBuilder.setPrice(
-    tx,
-    updateRequest,
-    priceFeed.priceFeedObjectId,
-    tx.pure([...Buffer.from(vaa, "base64")]),
-    coinType
-  );
-}
+// export const updatePythPriceForRequest = async (tx: SuiTxBlock, updateRequest: SuiTxArg, coinType: string) => {
+//   const priceFeed = getPythPriceFeed(coinType);
+//   const [vaa] = await getVaas([priceFeed.priceFeedId]);
+//   pythRuleTxBuilder.setPrice(
+//     tx,
+//     updateRequest,
+//     priceFeed.priceFeedObjectId,
+//     tx.pure([...Buffer.from(vaa, "base64")]),
+//     coinType
+//   );
+// }
 
 export const updateSwitchboardPriceForRequest = (tx: SuiTxBlock, updateRequest: SuiTxArg, coinType: string) => {
   const aggregator = getSwitchboardAggregator(coinType);
@@ -33,38 +32,38 @@ export const updateSwitchboardPriceForRequest = (tx: SuiTxBlock, updateRequest: 
 
 export const updatePrice = async (tx: SuiTxBlock, coinType: string) => {
   const request = xOracleTxBuilder.priceUpdateRequest(tx, coinType);
-  await updatePythPriceForRequest(tx, request, coinType);
   updateSwitchboardPriceForRequest(tx, request, coinType);
   xOracleTxBuilder.confirmPriceUpdateRequest(tx, request, coinType);
 }
 
-const getPythPriceFeed = (coinType: string) => {
-  if (coinType == testCoinTypes.btc) {
-    return pythTestnetIds.priceFeeds.btc_usd;
-  } else if (coinType === testCoinTypes.eth) {
-    return pythTestnetIds.priceFeeds.eth_usd;
-  } else if (coinType === testCoinTypes.usdc) {
-    return pythTestnetIds.priceFeeds.usdc_usd;
-  } else if (coinType === testCoinTypes.usdt) {
-    return pythTestnetIds.priceFeeds.usdt_usd;
-  } else if (coinType === '0x2::sui::SUI') {
-    return pythTestnetIds.priceFeeds.sui_usd;
-  } else {
-    throw new Error(`Unsupported coin type: ${coinType} for pyth price feed`);
-  }
-}
+// const getPythPriceFeed = (coinType: string) => {
+//   if (coinType == testCoinTypes.btc) {
+//     return pythTestnetIds.priceFeeds.btc_usd;
+//   } else if (coinType === testCoinTypes.eth) {
+//     return pythTestnetIds.priceFeeds.eth_usd;
+//   } else if (coinType === testCoinTypes.usdc) {
+//     return pythTestnetIds.priceFeeds.usdc_usd;
+//   } else if (coinType === testCoinTypes.usdt) {
+//     return pythTestnetIds.priceFeeds.usdt_usd;
+//   } else if (coinType === '0x2::sui::SUI') {
+//     return pythTestnetIds.priceFeeds.sui_usd;
+//   } else {
+//     throw new Error(`Unsupported coin type: ${coinType} for pyth price feed`);
+//   }
+// }
 
 const getSwitchboardAggregator = (coinType: string) => {
+  const switchboardTestnetIds = require(path.join(__dirname, "./switchboard_rule/vendors/switchboard_std_test/switchboard-oracle.testnet.json"));
   if (coinType == testCoinTypes.btc) {
-    return switchboardTestnetIds.aggregators.btc_usd;
+    return switchboardTestnetIds.btc_usd;
   } else if (coinType === testCoinTypes.eth) {
-    return switchboardTestnetIds.aggregators.eth_usd;
+    return switchboardTestnetIds.eth_usd;
   } else if (coinType === testCoinTypes.usdc) {
-    return switchboardTestnetIds.aggregators.usdc_usd;
+    return switchboardTestnetIds.usdc_usd;
   } else if (coinType === testCoinTypes.usdt) {
-    return switchboardTestnetIds.aggregators.usdt_usd;
+    return switchboardTestnetIds.usdt_usd;
   } else if (coinType === '0x2::sui::SUI') {
-    return switchboardTestnetIds.aggregators.sui_usd;
+    return switchboardTestnetIds.sui_usd;
   } else {
     throw new Error(`Unsupported coin type: ${coinType} for switchboard aggregator`);
   }
--- contracts/sui_x_oracle/supra_rule/index.ts
+++ contracts/sui_x_oracle/supra_rule/index.ts
@@ -4,3 +4,5 @@ import { SupraRuleTxBuilder } from "./typescript/tx-builder";
 
 const publishResult = require(path.join(__dirname, `./publish-result.${networkType}.json`));
 export const supraRuleTxBuilder = new SupraRuleTxBuilder(publishResult.packageId, publishResult.supraRegistryId, publishResult.supraRegistryCapId);
+
+export const supraRuleStructType =  `${publishResult.packageId}::rule::Rule`;

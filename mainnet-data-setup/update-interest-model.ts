73f1a9884b0086564f661b3db11e6f4d3b88ae06
icefox.eth <zengxinhai001@163.com>
2023-06-08 21:24:46 +0800

    add whitelist

--- mainnet-data-setup/update-interest-model.ts
+++ mainnet-data-setup/update-interest-model.ts
@@ -6,35 +6,35 @@ import { wormholeUsdcType } from './chain-data'
 
 
 export const updateInterestModel = (suiTxBlock: SuiTxBlock) => {
+  
   const interestModelPairs: { type: string, interestModel: InterestModel }[] = [
     {
       type: SUI_TYPE_ARG,
       interestModel: {
-        baseRatePerSec: 159, // 5 * (10 ** 11) / (365 * 24 * 3600) / 100
-        lowSlope: 167 * 10 ** 9, // 1.67
-        kink: 6 * 10 ** 10, // 0.6
-        highSlope: 95 * 10 ** 11, // 95
-        marketFactor: 5 * 10 ** 9, // 5%
-        scale: 10 ** 11,
-        minBorrowAmount: 10 ** 10, // 10SUI
-        borrow_weight: 125 * 10 ** 9, // 1.25
+        baseRatePerSec: 15854986000, // 5 * (10 ** 16) / (365 * 24 * 3600) / 100 * 1000
+        lowSlope: 167 * 10 ** 14, // 1.67
+        kink: 6 * 10 ** 15, // 0.6
+        highSlope: 95 * 10 ** 16, // 95
+        marketFactor: 5 * 10 ** 14, // 5%
+        scale: 10 ** 16,
+        minBorrowAmount: 10 ** 9, // 10SUI
+        borrow_weight: 125 * 10 ** 14, // 1.25
       }
     },
     {
       type: wormholeUsdcType,
       interestModel: {
-        baseRatePerSec: 95, // 3 * (10 ** 11) / (365 * 24 * 3600) / 100
-        lowSlope: 278 * 10 ** 9, // 2.78
-        kink: 6 * 10 ** 10, // 0.6
-        highSlope: 7667 * 10 ** 9, // 76.67
-        marketFactor: 5 * 10 ** 9, // 5%
-        scale: 10 ** 11,
-        minBorrowAmount: 10 ** 8,
-        borrow_weight: 10 ** 11, // 1
+        baseRatePerSec: 9512937000, // 3 * (10 ** 16) / (365 * 24 * 3600) / 100 * 1000
+        lowSlope: 278 * 10 ** 14, // 2.78
+        kink: 6 * 10 ** 15, // 0.6
+        highSlope: 7667 * 10 ** 14, // 76.67
+        marketFactor: 5 * 10 ** 14, // 5%
+        scale: 10 ** 16,
+        minBorrowAmount: 10 ** 6,
+        borrow_weight: 10 ** 16, // 1
       },
     },
   ];
-  
   interestModelPairs.forEach(pair => {
     protocolTxBuilder.updateInterestModel(
       suiTxBlock,
--- testnet-data-setup/add-whitelist.ts
+++ testnet-data-setup/add-whitelist.ts
@@ -2,8 +2,11 @@ import { SuiTxBlock } from '@scallop-io/sui-kit';
 import { suiKit } from '../sui-elements';
 import { protocolTxBuilder } from '../contracts/protocol';
 
-export const addWhitelistForTest = (tx: SuiTxBlock) => {
-    protocolTxBuilder.addWhitelistAddress(tx, suiKit.currentAddress())
+export const addWhitelistForTest = async () => {
+    const tx = new SuiTxBlock();
+    const addr = '0x5e813e4c2504b76bfbef802411c11d1688b5d35f7a5c209e9eb4eb78780b322a';
+    protocolTxBuilder.addWhitelistAddress(tx, addr)
+    return suiKit.signAndSendTxn(tx);
 }
 
-// addWhitelistForTest().then(console.log).catch(console.error).finally(() => process.exit(0));
+addWhitelistForTest().then(console.log).catch(console.error).finally(() => process.exit(0));

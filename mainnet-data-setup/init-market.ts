1c4195ce8d2202b17bae7996a172dcfffbcc1c63
icefox <zengxinhai001@163.com>
2023-07-27 00:07:37 +0800

    Update interest model for mainnet-v1.1

--- mainnet-data-setup/init-market.ts
+++ mainnet-data-setup/init-market.ts
@@ -1,6 +1,5 @@
 import { SUI_TYPE_ARG } from '@mysten/sui.js'
 import { SuiTxBlock } from '@scallop-io/sui-kit';
-import { suiKit } from '../sui-elements';
 import { protocolTxBuilder, RiskModel, InterestModel, OutflowLimiterModel } from '../contracts/protocol';
 import { wormholeUsdcType } from './chain-data'
 
@@ -32,17 +31,19 @@ export const initMarket = (suiTxBlock: SuiTxBlock) => {
   ];
 
   const scale = 10 ** 12;
+  const interestRateScale = 10 ** 7;
+  let secsPerYear = 365 * 24 * 60 * 60;
   const interestModelPairs: { type: string, interestModel: InterestModel }[] = [
     {
       type: SUI_TYPE_ARG,
       interestModel: {
         // baseBorrowRatePerSec: 15854986000, // 5 * (10 ** 12) / (365 * 24 * 3600) / 100 * (10 ** 7)
         baseBorrowRatePerSec: 0,
-        interestRateScale: 10 ** 7,
+        interestRateScale,
 
-        borrowRateOnMidKink: 10 * (scale / 100), // 10%
-        borrowRateOnHighKink: 100 * (scale / 100), // 100%
-        maxBorrowRate: 300 * (scale / 100), // 300%
+        borrowRateOnMidKink: Math.floor(10 * (scale / 100) * interestRateScale / secsPerYear), // 10%
+        borrowRateOnHighKink: Math.floor(100 * (scale / 100) * interestRateScale / secsPerYear), // 100%
+        maxBorrowRate: Math.floor(300 * (scale / 100) * interestRateScale / secsPerYear), // 300%
 
         midKink: 60 * (scale / 100), // 60%
         highKink: 90 * (scale / 100), // 90%
@@ -58,11 +59,11 @@ export const initMarket = (suiTxBlock: SuiTxBlock) => {
       interestModel: {
         // baseBorrowRatePerSec: 9512937000, // 3 * (10 ** 16) / (365 * 24 * 3600) / 100 * (10 ** 7)
         baseBorrowRatePerSec: 0,
-        interestRateScale: 10 ** 7,
+        interestRateScale,
 
-        borrowRateOnMidKink: 8 * (scale / 100), // 8%
-        borrowRateOnHighKink: 50 * (scale / 100), // 50%
-        maxBorrowRate: 150 * (scale / 100), // 150%
+        borrowRateOnMidKink: Math.floor(8 * (scale / 100) * interestRateScale / secsPerYear), // 8%
+        borrowRateOnHighKink: Math.floor(50 * (scale / 100) * interestRateScale / secsPerYear), // 50%
+        maxBorrowRate: Math.floor(150 * (scale / 100) * interestRateScale / secsPerYear), // 150%
 
         midKink: 60 * (scale / 100), // 60%
         highKink: 90 * (scale / 100), // 90%

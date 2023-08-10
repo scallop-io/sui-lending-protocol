719ce307dbd0c7578be2fa787c808c529e6d1a29
icefox <zengxinhai001@163.com>
2023-08-10 19:06:00 +0800

    disable supra and switchboard for now

--- contracts/sui_x_oracle/index.ts
+++ contracts/sui_x_oracle/index.ts
@@ -1,4 +1,4 @@
 export { xOracleTxBuilder } from "./x_oracle"
-export { switchboardRuleTxBuilder, switchboardRuleStructType, switchboardOracleData } from "./switchboard_rule"
-export { supraRuleTxBuilder, supraRuleStructType, supraOracleData } from "./supra_rule"
+// export { switchboardRuleTxBuilder, switchboardRuleStructType, switchboardOracleData } from "./switchboard_rule"
+// export { supraRuleTxBuilder, supraRuleStructType, supraOracleData } from "./supra_rule"
 export { pythRuleTxBuilder, pythRuleStructType, pythOracleData } from "./pyth_rule"

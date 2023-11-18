61d4b131e1cfe8b8f932e5c827e1c454a6fe2d37
icefox <zengxinhai001@163.com>
2023-11-18 19:34:10 +0800

    Add script for updating borrow fee

---
+++ mainnet-data-setup/update-borrow-fee.ts
@@ -0,0 +1,32 @@
+import { SuiTxBlock } from '@scallop-io/sui-kit';
+import {
+  protocolTxBuilder,
+  BorrowFee,
+} from '../contracts/protocol';
+import { borrowFees } from './borrow-fee';
+import { coinTypes } from './chain-data';
+import { buildMultiSigTx } from './multi-sig';
+
+
+export const updateBorrowFee = () => {
+  const suiTxBlock = new SuiTxBlock();
+
+  const borrowFeePairs: { type: string, borrowFee: BorrowFee }[] = [
+    {type: coinTypes.sui, borrowFee: borrowFees.sui},
+    {type: coinTypes.afSui, borrowFee: borrowFees.afSui},
+    {type: coinTypes.haSui, borrowFee: borrowFees.haSui},
+    {type: coinTypes.vSui, borrowFee: borrowFees.vSui},
+    {type: coinTypes.cetus, borrowFee: borrowFees.cetus},
+    {type: coinTypes.wormholeUsdc, borrowFee: borrowFees.wormholeUsdc},
+    {type: coinTypes.wormholeUsdt, borrowFee: borrowFees.wormholeUsdt},
+    {type: coinTypes.wormholeEth, borrowFee: borrowFees.wormholeEth},
+  ];
+
+  borrowFeePairs.forEach(pair => {
+    protocolTxBuilder.updateBorrowFee(suiTxBlock, pair.borrowFee, pair.type);
+  });
+
+  return buildMultiSigTx(suiTxBlock);
+}
+
+updateBorrowFee().then(console.log).catch(console.error).finally(() => process.exit(0));

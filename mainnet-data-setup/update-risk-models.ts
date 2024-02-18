c0d22713074e050fa2139812f8e7d8d25a71ef84
Nathan Ramli <hezkyanatanael84@gmail.com>
2024-02-18 22:56:26 +0800

    fix: update risk model

--- mainnet-data-setup/update-risk-models.ts
+++ mainnet-data-setup/update-risk-models.ts
@@ -2,12 +2,12 @@ import { SuiTxBlock } from '@scallop-io/sui-kit';
 import { protocolTxBuilder } from 'contracts/protocol';
 import { coinTypes } from './chain-data';
 import { buildMultiSigTx } from './multi-sig';
-import { cetusRiskModel } from './risk-models';
+import { afSuiRiskModel } from './risk-models';
 
 function updateRiskModels() {
   const tx = new SuiTxBlock();
   // update the risk model for 'sui'
-  protocolTxBuilder.updateRiskModel(tx, cetusRiskModel, coinTypes.cetus);
+  protocolTxBuilder.updateRiskModel(tx, afSuiRiskModel, coinTypes.afSui);
   return buildMultiSigTx(tx);
 }
 
--- sui-elements/index.ts
+++ sui-elements/index.ts
@@ -7,7 +7,6 @@ dotenv.config();
 export const secretKey = process.env.SECRET_KEY || '';
 export const networkType = (process.env.SUI_NETWORK_TYPE || 'testnet') as NetworkType;
 
-const shinamiNode = 'https://api.shinami.com/node/v1/sui_mainnet_1b23720b876e244bf1b2d28fd86e0d28';
-export const suiKit = new SuiKit({ secretKey, networkType, fullnodeUrls: [shinamiNode] });
+export const suiKit = new SuiKit({ secretKey, networkType });
 
 export const packagePublisher = new SuiAdvancePackagePublisher({ networkType });

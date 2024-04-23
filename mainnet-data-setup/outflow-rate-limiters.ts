ea1714480d8fad0d755012d1978aa154cdd6f6a5
icefox <zengxinhai001@163.com>
2024-04-23 12:05:16 +0800

    Update scripts

--- mainnet-data-setup/outflow-rate-limiters.ts
+++ mainnet-data-setup/outflow-rate-limiters.ts
@@ -9,17 +9,17 @@ const outflowSegmentDuration = 60 * 30 // 30 minutes
 
 export const outflowRateLimiters: Record<SupportedBaseAssets, OutflowLimiterModel> = {
   sui: {
-    outflowLimit: 10 ** (6 + coinDecimals.sui), // 1 million SUI per day
+    outflowLimit: 3 * 10 ** (6 + coinDecimals.sui), // 3 million SUI per day
     outflowCycleDuration,
     outflowSegmentDuration,
   },
   afSui: {
-    outflowLimit: 10 ** (4 + coinDecimals.afSui), // 10k afSUI per day
+    outflowLimit: 10 ** (6 + coinDecimals.afSui), // 1 million afSUI per day
     outflowCycleDuration,
     outflowSegmentDuration,
   },
   haSui: {
-    outflowLimit: 10 ** (4 + coinDecimals.haSui), // 10k haSUI per day
+    outflowLimit: 10 ** (6 + coinDecimals.haSui), // 1 million haSUI per day
     outflowCycleDuration,
     outflowSegmentDuration,
   },
@@ -39,17 +39,17 @@ export const outflowRateLimiters: Record<SupportedBaseAssets, OutflowLimiterMode
     outflowSegmentDuration,
   },
   wormholeEth: {
-    outflowLimit: 10 ** (3 + coinDecimals.wormholeEth), // 1000 ETH per day
+    outflowLimit: 5 * 10 ** (3 + coinDecimals.wormholeEth), // 5000 ETH per day
     outflowCycleDuration,
     outflowSegmentDuration,
   },
   wormholeUsdc: {
-    outflowLimit: 10 ** (6 + coinDecimals.wormholeUsdc), // 1 million USDC per day
+    outflowLimit: 3 * 10 ** (6 + coinDecimals.wormholeUsdc), // 3 million USDC per day
     outflowCycleDuration,
     outflowSegmentDuration,
   },
   wormholeUsdt: {
-    outflowLimit: 10 ** (6 + coinDecimals.wormholeUsdt), // 1 million USDT per day
+    outflowLimit: 3 * 10 ** (6 + coinDecimals.wormholeUsdt), // 3 million USDT per day
     outflowCycleDuration,
     outflowSegmentDuration,
   },

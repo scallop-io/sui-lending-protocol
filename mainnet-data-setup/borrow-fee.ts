5b642f1a0b91062aad51889856523a2ac7ec57a8
Nathan Ramli <hezkyanatanael84@gmail.com>
2024-04-29 17:33:11 +0700

    chore: update borrow fee

--- mainnet-data-setup/borrow-fee.ts
+++ mainnet-data-setup/borrow-fee.ts
@@ -2,13 +2,13 @@ import { SupportedBaseAssets } from "./chain-data";
 import type { BorrowFee } from 'contracts/protocol';
 
 export const borrowFees: Record<SupportedBaseAssets, BorrowFee> = {
-  sui: { numerator: 1, denominator: 1000 },
-  sca: { numerator: 1, denominator: 1000 },
-  cetus: { numerator: 1, denominator: 1000 },
-  afSui: { numerator: 1, denominator: 1000 },
-  haSui: { numerator: 1, denominator: 1000 },
-  vSui: { numerator: 1, denominator: 1000 },
-  wormholeEth: { numerator: 1, denominator: 1000 },
-  wormholeUsdc: { numerator: 1, denominator: 1000 },
-  wormholeUsdt: { numerator: 1, denominator: 1000 },
+  sui: { numerator: 3, denominator: 1000 },
+  sca: { numerator: 3, denominator: 1000 },
+  cetus: { numerator: 3, denominator: 1000 },
+  afSui: { numerator: 3, denominator: 1000 },
+  haSui: { numerator: 3, denominator: 1000 },
+  vSui: { numerator: 3, denominator: 1000 },
+  wormholeEth: { numerator: 3, denominator: 1000 },
+  wormholeUsdc: { numerator: 3, denominator: 1000 },
+  wormholeUsdt: { numerator: 3, denominator: 1000 },
 }

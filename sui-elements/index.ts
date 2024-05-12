1a9bba9dfcd7ac5c1df3ca58d5f9a9972754b3b1
Nathan Ramli <hezkyanatanael84@gmail.com>
2024-05-12 20:49:24 +0700

    chore: fix typo

--- sui-elements/index.ts
+++ sui-elements/index.ts
@@ -4,12 +4,10 @@ import { SuiAdvancePackagePublisher } from "@scallop-io/sui-package-kit";
 
 dotenv.config();
 
-export const secretKey = process.env.L_SECRET_KEY || '';
+export const secretKey = process.env.SECRET_KEY || '';
 export const networkType = (process.env.SUI_NETWORK_TYPE || 'testnet') as NetworkType;
 
-const fullNode = 'https://api.shinami.com/node/v1/sui_mainnet_8a6507ca04e7ba4cdc713a9d66e9d54a';
-
-export const suiKit = new SuiKit({ secretKey, networkType, fullnodeUrls: [fullNode] });
+export const suiKit = new SuiKit({ secretKey, networkType });
 
 console.log(suiKit.currentAddress());
 

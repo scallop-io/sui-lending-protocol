d01ea9b99d4fec54d9fbd34432df860a17200ce3
icefox <zengxinhai001@163.com>
2024-05-22 21:46:52 +0800

    Supply limits

--- mainnet-data-setup/supply-limits.ts
+++ mainnet-data-setup/supply-limits.ts
@@ -5,13 +5,13 @@ import {
 
 
 export const SupplyLimits: Record<SupportedBaseAssets, number> = {
-  sui: 1e9 * Math.pow(10, coinDecimals.sui),
-  wormholeUsdc: 1e9 * Math.pow(10, coinDecimals.wormholeUsdc),
-  wormholeUsdt: 1e9 * Math.pow(10, coinDecimals.wormholeUsdt),
-  sca: 1e8 * Math.pow(10, coinDecimals.sca),
-  afSui: 1e8 * Math.pow(10, coinDecimals.afSui),
-  haSui: 1e8 * Math.pow(10, coinDecimals.haSui),
+  sui: 1e8 * Math.pow(10, coinDecimals.sui),
+  wormholeUsdc: 1e8 * Math.pow(10, coinDecimals.wormholeUsdc),
+  wormholeUsdt: 1e8 * Math.pow(10, coinDecimals.wormholeUsdt),
+  sca: 1e7 * Math.pow(10, coinDecimals.sca),
+  afSui: 1e7 * Math.pow(10, coinDecimals.afSui),
+  haSui: 1e7 * Math.pow(10, coinDecimals.haSui),
   vSui: 1e7 * Math.pow(10, coinDecimals.vSui),
   cetus: 2e6 * Math.pow(10, coinDecimals.cetus),
-  wormholeEth: 1e5 * Math.pow(10, coinDecimals.wormholeEth),
+  wormholeEth: 1e4 * Math.pow(10, coinDecimals.wormholeEth),
 }

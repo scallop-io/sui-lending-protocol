0b22423c971fe9a68ac5c6a09e852ba14056e891
icefox <zengxinhai001@163.com>
2024-12-14 20:17:18 +0800

    Update script for new pool

--- mainnet-data-setup/add-new-pool.ts
+++ mainnet-data-setup/add-new-pool.ts
@@ -16,7 +16,7 @@ import { decimalsRegistryTxBuilder } from 'contracts/libs/coin_decimals_registry
 import { coinTypes, coinMetadataIds } from './chain-data';
 import { buildMultiSigTx } from './multi-sig';
 
-async function addNewPool() {
+async function addNewPool_DEEP() {
   const tx = new SuiTxBlock();
   const coin = 'deep';
   const dustCoinId = '0x186ee7ad0a9a8668fa62e4d524d8738303463b6d03b30f106c54594f0ed9263b'; // This is used to keep a minimum amount of the coin in the pool
@@ -40,4 +40,28 @@ async function addNewPool() {
   return buildMultiSigTx(tx);
 }
 
-addNewPool().then(console.log);
+async function addNewPool_FUD() {
+  const tx = new SuiTxBlock();
+  const coin = 'fud';
+  const dustCoinId = '0x5a968bd1dcf38f0e13ede7da9af7de0e8d92f63952f017b5f5ed7ac86726b882'; // This is used to keep a minimum amount of the coin in the pool
+  const coinType = coinTypes[coin];
+  protocolTxBuilder.addInterestModel(tx, interestModels[coin], coinType);
+  protocolTxBuilder.addLimiter(tx, outflowRateLimiters[coin], coinType);
+  protocolTxBuilder.setSupplyLimit(tx, SupplyLimits[coin], coinType);
+  protocolTxBuilder.updateBorrowFee(tx, borrowFees[coin], coinType);
+  protocolTxBuilder.setFlashloanFee(tx, FlashloanFees[coin], coinType);
+  protocolTxBuilder.setIncentiveRewardFactor(tx, incentiveRewardFactors[coin], coinType);
+
+  pythRuleTxBuilder.registerPythPriceInfoObject(tx, oracles[coin].pythPriceObjectId, coinType);
+
+  decimalsRegistryTxBuilder.registerDecimals(tx, coinMetadataIds[coin], coinType);
+
+  // Burn dust to keep a minimum amount of the coin in the pool
+  const dustToBurn = protocolTxBuilder.supplyBaseAsset(tx, dustCoinId, coinType);
+  const voidAddress = '0x0';
+  tx.transferObjects([dustToBurn], voidAddress);
+
+  return buildMultiSigTx(tx);
+}
+
+addNewPool_DEEP().then(console.log);

b64a26276e7855ed20dc7fb34e0e43caa1065cb2
Nathan Ramli <hezkyanatanael84@gmail.com>
2024-12-18 01:28:30 +0800

    chore: update borrow and supply limits

--- mainnet-data-setup/supply-limits.ts
+++ mainnet-data-setup/supply-limits.ts
@@ -18,6 +18,6 @@ export const SupplyLimits: Record<SupportedBaseAssets, number> = {
   wormholeSol: 2e4 * Math.pow(10, coinDecimals.wormholeSol),
   nativeUsdc: 5e7 * Math.pow(10, coinDecimals.nativeUsdc),
   sbEth: 5e3 * Math.pow(10, coinDecimals.sbEth),
-  deep: 1e7 * Math.pow(10, coinDecimals.deep),
-  fud: 4e11 * Math.pow(10, coinDecimals.fud),
+  deep: 25e6 * Math.pow(10, coinDecimals.deep), // 25M
+  fud: 1e12 * Math.pow(10, coinDecimals.fud),
 }

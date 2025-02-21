d560fff0613e3477c612c363aa826ca726f67812
nathanramli <hezkyanatanael84@gmail.com>
2025-02-21 14:42:10 +0700

    chore: update musd and hasui limits

--- mainnet-data-setup/set-borrow-limits.ts
+++ mainnet-data-setup/set-borrow-limits.ts
@@ -6,18 +6,30 @@ import { coinTypes } from './chain-data';
 import { BorrowLimits } from './borrow-limits';
 import { suiKit } from 'sui-elements';
 import { buildMultiSigTx } from './multi-sig';
+import { SupplyLimits } from './supply-limits';
 
 
 export const updateBorrowLimits = async () => {
     const suiTxBlock = new SuiTxBlock();
 
     const borrowLimitList: { type: string, limit: number }[] = [
-        { type: coinTypes.deep, limit: BorrowLimits.deep },
+        { type: coinTypes.mUsd, limit: BorrowLimits.mUsd },
+        { type: coinTypes.haSui, limit: BorrowLimits.haSui },
     ];
 
     borrowLimitList.forEach(pair => {
         protocolTxBuilder.setBorrowLimit(suiTxBlock, pair.limit, pair.type);
     });
+
+
+  const supplyLimitList: { type: string, limit: number }[] = [
+    {type: coinTypes.mUsd, limit: SupplyLimits.mUsd},
+    {type: coinTypes.haSui, limit: SupplyLimits.haSui},
+  ];
+
+  supplyLimitList.forEach(pair => {
+    protocolTxBuilder.setSupplyLimit(suiTxBlock, pair.limit, pair.type);
+  });    
 
     const txBytes = await buildMultiSigTx(suiTxBlock);
     const resp = await suiKit.provider().dryRunTransactionBlock({

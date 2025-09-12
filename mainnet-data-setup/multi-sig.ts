1ec8e1b4ff1e0972a97a3a7956b2decc3866a655
icefox <zengxinhai001@163.com>
2025-09-12 22:26:33 +0800

    Add multi-sig info

--- mainnet-data-setup/multi-sig.ts
+++ mainnet-data-setup/multi-sig.ts
@@ -1,6 +1,6 @@
 import { SuiTxBlock, toBase64 } from '@scallop-io/sui-kit';
 import { suiKit } from 'sui-elements';
-export const MULTI_SIG_ADDRESS = '0x1226a80ef40bd2e70c6a285b045b9b5d29915a2c5a2d57a2d3032cbdd89a8d5c';
+export const MULTI_SIG_ADDRESS = '0x4f8744837e95c79258a23ea6dc1985dcbbd39935a49b75e3d43a4427ff5f5cb4';
 
 export async function buildMultiSigTx(tx: SuiTxBlock) {
   tx.setSender(MULTI_SIG_ADDRESS);
